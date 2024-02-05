9cd1484065c1f3cc8ad929b576210b2ba1becd5c
shumcoder <g16052015@mail.ru>
2024-02-05 17:43:23 +0300

    errors

--- main.py
+++ main.py
@@ -21,6 +21,16 @@ def start():
             return
     canva.after(50,start)
 
+def balls_func():
+    balls = [
+        canva.create_oval(350, 250, 380, 280, fill="red"),
+        canva.create_oval(350, 350, 380, 380, fill="green"),
+        canva.create_oval(350, 450, 380, 480, fill="blue"),
+        canva.create_oval(350, 550, 380, 580, fill="#fff")
+    ]
+    return  balls
+def restart():
+    return balls_func()
 
 canva = Canvas(width=500,height=700)
 canva.pack()
@@ -31,21 +41,16 @@ for i in range(16):
     color = random.choice(random_color)
     canva.create_oval(20+i*30,50,40+i*30,70,fill=color,tags="ball"+str(i))
 
-
-
-
 canva.create_line(0,50,500,50)
 canva.create_text(250,150,text='Нажми, для начала праздника',tags='start')
 canva.tag_bind('start','<Button-1>',click)
 
-balls = [
-    canva.create_oval(350,250,380,280,fill="red"),
-    canva.create_oval(350,350,380,380,fill="green"),
-    canva.create_oval(350,450,380,480,fill="blue"),
-    canva.create_oval(350,550,380,580,fill="#fff")
-]
+balls = balls_func()
 
 btn_start = Button(text="start",font="Arial 15",command=start)
 btn_start.place(x=400,y=200)
+
+btn_re_start = Button(text="restart",font="Arial 15",command=restart)
+btn_re_start.place(x=200,y=200)
 
 tk.mainloop()
