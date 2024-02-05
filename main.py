1bc70b03e1a66a400a5fbf3f0f9c64ad43a71b20
shumcoder <g16052015@mail.ru>
2024-02-05 16:33:35 +0300

    ЭЧемпионат шаров

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-from tkinter import *
+
 import random
+from tkinter import *
+from tkinter import messagebox
 tk = Tk()
 def click(event):
     while True:
@@ -9,14 +11,18 @@ def click(event):
             canva.update()
             canva.after(50)
 
-def move_ball1():
+def start():
     step = random.randint(1,20)
-    canva.move(ball1,step,0)
-    if canva.coords(ball1)[2]<500:
-        canva.after(50,move_ball1)
+    for ball in balls:
+        canva.move(ball,step,0)
+        if canva.coords(ball)[2]>=500:
+            winner_color = canva.itemcget(ball, 'fill')
+            messagebox.showinfo('Победа',f'Победил шар {winner_color}')
+            return
+    canva.after(50,start)
 
 
-canva = Canvas(width=500,height=500)
+canva = Canvas(width=500,height=700)
 canva.pack()
 
 random_color=["red","blue","green"]
@@ -25,11 +31,21 @@ for i in range(16):
     color = random.choice(random_color)
     canva.create_oval(20+i*30,50,40+i*30,70,fill=color,tags="ball"+str(i))
 
+
+
+
 canva.create_line(0,50,500,50)
 canva.create_text(250,150,text='Нажми, для начала праздника',tags='start')
 canva.tag_bind('start','<Button-1>',click)
 
-ball1 = canva.create_oval(350,350,380,380,fill="red")
-move_ball1()
+balls = [
+    canva.create_oval(350,250,380,280,fill="red"),
+    canva.create_oval(350,350,380,380,fill="green"),
+    canva.create_oval(350,450,380,480,fill="blue"),
+    canva.create_oval(350,550,380,580,fill="#fff")
+]
+
+btn_start = Button(text="start",font="Arial 15",command=start)
+btn_start.place(x=400,y=200)
 
 tk.mainloop()
